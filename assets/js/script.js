4caddf55cbe92a310b6d2626dc94a35ed0f68fe2
Jeka98 <Zhukjeka1998@yandex.by>
2018-11-04 13:30:50 +0300

    feat: add my slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,32 @@
-$(document).ready(function() {
-    $('#carousel').owlCarousel({
-        loop:true,
-        nav:false, 
-        smartSpeed:1000, 
-        autoplayTimeout:2000,
-        items:1
+const commentsContent = [
+    {
+        commentText: '"Suspendisse tempor turpis odio, sit amet cursus leo consequat non. Maecenas lacinia faucibus enimqui interdum dolor pulvinar vitae"',
+        author: 'John Doe 1',
+    },
+    {
+        commentText: '"Suspendisse tempor turpis odio, sit amet cursus leo consequat non. Maecenas lacinia faucibus enimqui interdum dolor pulvinar vitae"',
+        author: 'John Doe 2',
+    },
+    {
+        commentText: '"Suspendisse tempor turpis odio, sit amet cursus leo consequat non. Maecenas lacinia faucibus enimqui interdum dolor pulvinar vitae"',
+        author: 'John Doe 3',
+    },
+];
+const slides = [...document.getElementById('slides').children];
+slides.forEach((item, id) => {
+    item.addEventListener('click', () => {
+        const comment = document.getElementById('comment');
+        const author = document.getElementById('author-name');
+
+        comment.innerHTML = commentsContent[id].commentText;
+        author.innerHTML = commentsContent[id].author;
+
+        slides.forEach((slide, index) => {
+            if (index === id) {
+                slide.style.backgroundColor = 'white';
+            } else {
+                slide.style.backgroundColor = 'transparent';
+            }
+        });
     });
 });
